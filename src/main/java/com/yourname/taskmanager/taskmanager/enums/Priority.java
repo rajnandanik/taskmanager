93e435c5b4fe9d0cec8823416ce31e238f0f1ed7
rajnandanik <162095650+rajnandanik@users.noreply.github.com>
2025-07-20 18:30:45 +0530

    Add files via upload

---
+++ src/main/java/com/yourname/taskmanager/taskmanager/enums/Priority.java
@@ -0,0 +1,7 @@
+package com.yourname.taskmanager.taskmanager.enums;
+
+public enum Priority {
+    LOW,
+    MEDIUM,
+    HIGH
+}
---
+++ src/main/java/com/yourname/taskmanager/taskmanager/enums/TaskStatus.java
@@ -0,0 +1,7 @@
+package com.yourname.taskmanager.taskmanager.enums;
+
+public enum TaskStatus {
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED
+}
